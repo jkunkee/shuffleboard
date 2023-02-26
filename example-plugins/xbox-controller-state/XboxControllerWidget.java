0311e369f1efbaa07432867ecb045b76b43c7565
Jon Kunkee <jonathan.kunkee@gmail.com>
2023-02-26 01:18:03 -0800

    Get minimum possible custom plugin builing

---
+++ example-plugins/xbox-controller-state/XboxControllerWidget.java
@@ -0,0 +1,6 @@
+
+@Requires(group = "edu.wpi.first.shuffleboard", name = "Base", minVersion = "1.0.0")
+@Description(group = "com.team4131.xboxcontrollerwidget", name = "XboxControllerWidget", version = "0.0.1", summary = "Displays the state of an Xbox controller")
+public class XboxControllerWidgetPlugin extends Plugin {
+
+}
