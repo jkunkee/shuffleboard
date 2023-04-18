f7acac0e6a71a4be1f0835a62deb0e21a29b4b00
Jon Kunkee <jonathan.kunkee@gmail.com>
2023-04-17 22:01:58 -0700

    Rename working example to SerialPlugin

--- example-plugins/serial-plugin/src/main/java/org/team4131/serial/SerialPlugin.java
+++ example-plugins/serial-plugin/src/main/java/org/team4131/serial/SerialPlugin.java
@@ -6,8 +6,8 @@ import edu.wpi.first.shuffleboard.api.plugin.Plugin;
 import com.fazecast.jSerialComm.SerialPort;
 
 @Requires(group = "edu.wpi.first.shuffleboard", name = "Base", minVersion = "1.0.0")
-@Description(group = "com.team4131.xboxcontrollerwidget", name = "XboxControllerWidget", version = "0.0.1", summary = "Displays the state of an Xbox controller")
-public class XboxControllerWidgetPlugin extends Plugin {
+@Description(group = "org.team4131.serial", name = "SerialPlugin", version = "0.0.1", summary = "Links NetworkTables events to PC-side serial events")
+public class SerialPlugin extends Plugin {
     void init(com.sun.source.util.JavacTask arg0, java.lang.String... arg1) {
         var foo = SerialPort.getCommPorts();
         System.out.println(foo);
